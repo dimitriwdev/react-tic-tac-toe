65af71b9c309dc55f0bc6fd6640aeb81a1d99fbd
dimitri devoille <dimitri.wdev@gmail.com>
2021-11-29 14:50:46 +0000

    add tic tac toe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Board from "./components/Board";
+import WinnerLogic from "./components/WinnerLogic";
 
-function App() {
+const App = () => {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [stepNumber, setStepNumber] = useState(0);
+  const [isXNext, setIsXNext] = useState(true);
+  const winner = WinnerLogic(history[stepNumber]);
+  const xO = isXNext ? "X" : "O";
+
+  const handleClick = (i) => {
+    const historyPoint = history.slice(0, stepNumber + 1);
+    const current = historyPoint[stepNumber];
+    const squares = [...current];
+
+    if (winner || squares[i]) return;
+
+    squares[i] = xO;
+    setHistory([...historyPoint, squares]);
+    setStepNumber(historyPoint.length);
+    setIsXNext(!isXNext);
+  };
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setIsXNext(step % 2 === 0);
+  };
+
+  // uncomment to displayed history
+
+  // const renderMoves = () =>
+  //   history.map((_step, move) => {
+  //     const destination = move ? `Go to move #${move}` : "Go to Start";
+  //     return (
+  //       <li key={move}>
+  //         <button onClick={() => jumpTo(move)}>{destination}</button>
+  //       </li>
+  //     );
+  //   });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Tic Tac Toe</h1>
+      <Board squares={history[stepNumber]} onClick={handleClick} />
+      <div className="infoWrapper">
+        {/* uncomment to displayed history */}
+        {/* <ul>{renderMoves()}</ul> */}
+        <h3>{winner ? "winner: " + winner : "Next player: " + xO}</h3>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
